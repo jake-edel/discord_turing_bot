ddd2e8b08de615f439222b048ba850d2c7b863af
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-18 11:46:08 -0700

    refactor: clean up starter code, accept input to print event from array of events

--- je_calendar.js
+++ je_calendar.js
@@ -1,29 +1,15 @@
+clear();
 
-var  exec = require('child_process').exec;
-exec('clear',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
-
-const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
+const fs = require('fs');
+const { google } = require('googleapis');
+const TOKEN_PATH = 'sec/token.json';
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  authorize(JSON.parse(content), listEvents);
+fs.readFile('sec/credentials.json', (err, content) => {
+	if (err) return console.log('Error loading client secret file:', err);
+	// Authorize a client with credentials, then call the Google Calendar API.
+	authorize(JSON.parse(content), listEvents);
 });
 
 /**
@@ -33,90 +19,90 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+	const { client_secret, client_id, redirect_uris } = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(
+		client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		oAuth2Client.setCredentials(JSON.parse(token));
+		callback(oAuth2Client);
+	});
 }
 
-// /**
-//  * Get and store new token after prompting for user authorization, and then
-//  * execute the given callback with the authorized OAuth2 client.
-//  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-//  * @param {getEventsCallback} callback The callback for the authorized client.
-//  */
-// function getAccessToken(oAuth2Client, callback) {
-//   const authUrl = oAuth2Client.generateAuthUrl({
-//     access_type: 'offline',
-//     scope: SCOPES,
-//   });
-//   console.log('Authorize this app by visiting this url:', authUrl);
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
-//   rl.question('Enter the code from that page here: ', (code) => {
-//     rl.close();
-//     oAuth2Client.getToken(code, (err, token) => {
-//       if (err) return console.error('Error retrieving access token', err);
-//       oAuth2Client.setCredentials(token);
-//       // Store the token to disk for later program executions
-//       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-//         if (err) return console.error(err);
-//         console.log('Token stored to', TOKEN_PATH);
-//       });
-//       callback(oAuth2Client);
-//     });
-//   });
-// }
-
 /**
- * Lists the next 10 events on the user's primary calendar.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-// var test = ''
+
 function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      // console.log(calendar.events)
-      console.log('Upcoming events:');
-      console.log(events[3]);
-      console.log('\n');
+	const calendar = google.calendar({version: 'v3', auth});
+	calendar.events.list({
+		calendarId: 'casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com',
+		timeMin: (new Date()).toISOString(),
+		maxResults: 10,
+		singleEvents: true,
+		orderBy: 'starttime',
+		// orderBy: 'updated',
+	}, (err, res) => {
+		if (err) return console.log('The API returned an error: ' + err);
+		const events = res.data.items;
+		if (events.length) {
+			print_header(res)
+			print_event(res)
 
-      console.log(`Event: ${events[3].summary}`);
-      console.log('\n');
+			// const start = new Date(events[3].start.dateTime);
+			// const end = new Date(events[3].end.dateTime);
 
-      var start = new Date(events[3].start.dateTime);
-      var end = new Date(events[3].end.dateTime);
+			// console.log(`${start} => ${end}`);
 
-      console.log(`${start} => ${end}`);
-      console.log('\n');
-      console.log(events[3].description.replace(/(<br>)/g, "\n"));
-      console.log('\n');
 
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        // test += `${start} - ${event.summary}\n`
-        // console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-    // console.log(test)
-  });
+			// events.map((event, i) => {
+			// const start = event.start.dateTime || event.start.date;
+			// test += `${start} - ${event.summary}\n`
+			// console.log(`${start} - ${event.summary}`);
+		// });
+		}
+		else {
+			console.log('No upcoming events found.');
+		}
+	});
+}
+
+function print_header(results) {
+	const calName = results.data.summary;
+	const numResults = results.config.params.maxResults
+
+	console.log(`\nReturning ${numResults} results, sorted by closest first, from ${calName}\n`)
+}
+
+function print_event(results) {
+	const events = results.data.items;
+	input_index(events)
+}
+function input_index(events) {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question(`Enter a number 1 to ${events.length} to view event: `, function(input) {
+		console.log(`\n${events[input - 1].summary}\n`);
+		check_description(events, input);
+		console.log('\n');
+		console.log(events[input - 1]);
+		rl.close();
+	});
+}
+
+function check_description(events, input) {
+	events[input - 1].description ? console.log(events[input - 1].description.replace(/(<br>)/g, '\n')) : null;
+}
+
+function clear() {
+	const exec = require('child_process').exec;
+	exec('clear', function (error, stdout, stderr) {
+		console.log(stdout);
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		}
+	});
 }
